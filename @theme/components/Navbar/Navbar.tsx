f9401d155f528d2d87868c073a64d994c09da94e
aim4ik11 <yaroslav.rusak@redocly.com>
2025-10-09 16:59:44 +0300

    fix: add border for editor navbar

--- @theme/components/Navbar/Navbar.tsx
+++ @theme/components/Navbar/Navbar.tsx
@@ -105,7 +105,7 @@ export function Navbar({ className }: NavbarProps): JSX.Element | null {
       isTop={prevScrollPos === 0}
       isColorful={colorfulPagesList.includes(pathname)}
       isDefault={defaultPageList.includes(pathname)}
-      isDocs={isDocsOrLearnPage}
+      isDocs={isDocsOrLearnPage || isEditorPage}
       showVSCodeExtensionBanner={showVSCodeExtensionBanner}
       isMobile={isOpen}
       isRespect={pathname === '/respect'}
