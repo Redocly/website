ee203aaf2dd7dad48749a74d4a8e5d40bdf1c2d6
Olena Pelena <olena.pelena@redocly.com>
2026-05-15 13:25:17 +0000

    chore: add developer marketer to the careers page

--- pages/careers/positions.ts
+++ pages/careers/positions.ts
@@ -1,20 +1,14 @@
 export const positions = [
   {
-    title: 'Product-Led Growth Manager',
-    locations: ['Ukraine'],
-    employmentType: 'Full time',
-    slug: 'product-led-growth-manager',
-  },
-  {
-    title: 'Data Engineer',
-    locations: ['Remote'],
-    employmentType: 'Full time',
-    slug: 'data-engineer',
-  },
-  {
     title: 'Software Engineer',
     locations: ['Ukraine', 'Poland'],
     employmentType: 'Full time',
     slug: 'software-engineer',
+  },
+ {
+    title: 'Developer Marketer (Video-Focused)',
+    locations: ['Lviv, Ukraine'],
+    employmentType: 'Full time',
+    slug: 'developer-marketer',
   },
 ];
