a075f5baa0ab439d82c5f59bf109877df0c14316
Roman Marshevskyi <marshevskyy@gmail.com>
2026-07-29 19:02:13 +0300

    docs: new ai reviewer page (#345)
    * docs: new ai reviewer page
    
    * chore: bump realm and marketing-pages
    
    * chore: adjust headers for loops and navbar
    
    * fix: remove Maintainer
    
    * chore: adjust Navbar and bump marketing-pages
    
    * chore: adjust Navbar
    
    ---------
    
    Co-authored-by: Daryna Pastushenko <darynapastushenko1207@gmail.com>

---
+++ pages/ai-reviewer/ai-reviewer.page.tsx
@@ -0,0 +1,10 @@
+import Page from '@redocly/marketing-pages/pages/ai-reviewer/ai-reviewer.page.js';
+
+export const frontmatter = {
+  seo: {
+    title: 'AI Reviewer',
+  },
+  slug: '/ai-reviewer',
+};
+
+export default Page;
