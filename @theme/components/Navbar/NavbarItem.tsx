4a3e3337b39bca43acc7e6be7f9e056c833ac24a
Daryna Pastushenko <77724851+Daryna-del@users.noreply.github.com>
2026-08-11 16:43:49 +0300

    chore: bump realm and marketing-pages versions (#381)

--- @theme/components/Navbar/NavbarItem.tsx
+++ @theme/components/Navbar/NavbarItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router';
 
 import type { ResolvedNavItem, ResolvedNavLinkItem, ResolvedNavGroupItem } from '@redocly/config';
 
