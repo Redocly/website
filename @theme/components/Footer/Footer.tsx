7c3a512faedc9f21101530c01cf64540c7f27d62
Daryna Pastushenko <daryna.pastushenko@redocly.com>
2026-08-11 16:33:21 +0300

    fix: broken build

--- @theme/components/Footer/Footer.tsx
+++ @theme/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { linearGradient } from 'polished';
-import { useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router';
 
 import type { ResolvedNavItem } from '@redocly/config';
 
